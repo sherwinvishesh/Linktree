cb28adbdf1b26a13679c08ccb9bac4e5ec34c304
Sherwin Vishesh Jathanna <60791187+sherwinvishesh@users.noreply.github.com>
2024-01-23 14:26:49 -0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,4 @@
-function addLink(text, href) {
-    const linkElement = document.createElement('div');
-    linkElement.className = 'link';
-    linkElement.innerHTML = `<a href="${href}">${text}</a>`;
-    document.querySelector('.container').insertBefore(linkElement, document.querySelector('.qr-code'));
-}
+function buttonAction(link) {
+    window.open(link, '_blank').focus();
 
-// Use this function to add new links
-// addLink('Link 3', 'https://example.com');
+}
